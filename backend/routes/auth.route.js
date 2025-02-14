3623b1fe20b9f9b29c6226bf82d8ad38998207ba
ashraf <sadarsign@gmail.com>
2025-02-14 22:53:44 +0300

    Initialized

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("Sign up route called");
-});
-router.get("/login", (req, res) => {
-    res.send("Login route called");
-});
-router.get("/logout", (req, res) => {
-    res.send("Logout route called");
-});
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -10,4 +11,6 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
-})
+
+    connectDB();
+});
