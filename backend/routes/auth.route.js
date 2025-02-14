155dc9594535b7623369ccc9337c915ab505d082
ashraf <sadarsign@gmail.com>
2025-02-14 22:16:46 +0300

    Initialized

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,5 +5,11 @@ const router = express.Router();
 router.get("/signup", (req, res) => {
     res.send("Sign up route called");
 });
+router.get("/login", (req, res) => {
+    res.send("Login route called");
+});
+router.get("/logout", (req, res) => {
+    res.send("Logout route called");
+});
 
 export default router;
