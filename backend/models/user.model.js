2afd5dbd07f2faaec2b9b4f680b1e0e8740229c4
ashraf <sadarsign@gmail.com>
2025-02-15 21:00:03 +0300

    Initialized

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userScema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "name is required"]
@@ -38,3 +39,26 @@ const userScema = new mongoose.Schema({
     {
         timestamps: true,
     })
+
+
+const user = mongoose.model("user", userSchema)
+
+//pre-save hook to hash password before saving to database
+userSchema.pre("save", async function (next) {
+    if (!this.isModified(password)) return next()
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+
+    }
+})
+//sulta 1234567
+//1234567 => invalid credentials 
+userSchema.methods.comparPassowrd = async function (password) { 
+    return bcrypt.compare(password, this.password);
+}
+
+export default user;
