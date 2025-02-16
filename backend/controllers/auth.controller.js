931c057d1ff84e43bf2dfff2a7755512a57ba64f
ashraf <sadarsign@gmail.com>
2025-02-16 20:06:13 +0300

    Initialized

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
                 email: user.email,
                 role: user.role,
             });
-        }else {
+        } else {
             res.status(401).json({ message: "invalid email or password" })
         }
     } catch (error) {
@@ -104,5 +104,32 @@ export const logout = async (req, res) => {
     } catch (error) {
         console.log("Error in logout controller", error.message)
         res.status(500).json({ message: "Server error", error: error.message })
+    }
+}
+
+export const refreshToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) {
+            return res.status(401).json({ message: "No refresh token provided" })
+        }
+        const decoded = jwt.verify(refreshToken, process.env[REFRESH_TOKEN_SECRET]);
+        const storedToken = await redis.get(`refresh_token: ${decoded.userId}`);
+
+        if (storedToken !== refreshToken) {
+            return res.status(401).json({ message: "Invalid refresh token" });
+        }
+
+        const accessToken = jwt.sign({ userId: decoded.userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 15 * 60 * 1000,
+        });
+
+        res.json({ message: "Token refreshed successfully" });
+    } catch (error) {
+
     }
 }
