342c1b2c3f83c970831ca2fc32a753070fdc3e6a
ashraf <sadarsign@gmail.com>
2025-02-16 17:46:46 +0300

    Initialized

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -100,6 +100,7 @@ export const logout = async (req, res) => {
         res.clearCookie("refreshToken");
         res.json({ message: "Logged out successfull" });
     } catch (error) {
+        console.log("Error in logout controller", error.message)
         res.status(500).json({ message: "Server error", error: error.message })
     }
 }
