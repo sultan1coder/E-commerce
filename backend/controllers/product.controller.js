7297cb9dc1c735defbe96150d05b8604b6dfdb51
ashraf <sadarsign@gmail.com>
2025-02-18 23:03:49 +0300

    Initialized

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,5 @@
+import { json } from "express";
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js"
 
 export const getAllProducts = async (req, res) => {
@@ -6,6 +8,31 @@ export const getAllProducts = async (req, res) => {
         res.json({ products })
     } catch (error) {
         console.log("Error in getAllProducts controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featured_products");
+        if (featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
+
+        //if not in redis, fetch from mongodb
+        featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+        if (!featuredProducts) {
+            return res.status(404).json({ message: "No featured products found" });
+        }
+
+        //store in redis for future quick access
+
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+        res.json(featuredProducts);
+    } catch (error) {
+        console.log("Error in getFeaturedProducts", error.message);
         res.status(500).json({ message: "Server error", error: error.message });
     }
 }
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import router from "./auth.route.js";
-import { getAllProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middlewares/auth.middleware.js";
 
 const Router = express.Router;
 
-router.get("/", protectRoute, adminRoute, getAllProducts)
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default Router;
