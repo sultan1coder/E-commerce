5a8fa1a81488561eb3328d3c81ef2d250df2b9b9
ashraf <sadarsign@gmail.com>
2025-02-18 19:40:45 +0300

    Initialized

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -5,9 +5,8 @@ export const protectRoute = async (req, res, next) => {
         const accessToken = req.cookies.accessToken;
 
         if (!accessToken) {
-            return res.status(401).json({ mesaage: "Unauthorized - No access token provided" });
+            return res.status(401).json({ message: "Unauthorized - No access token provided" });
         }
-
         try {
             const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
             const user = await User.findById(decoded.userId).select("-password");
@@ -17,8 +16,8 @@ export const protectRoute = async (req, res, next) => {
             }
 
             req.user = user;
-
             next();
+
         } catch (error) {
             if (error.name === "TokenExpiredError") {
                 return res.status(401).json({ message: "Unauthorized - Access token expired" });
@@ -33,9 +32,9 @@ export const protectRoute = async (req, res, next) => {
 
 
 export const adminRoute = (req, res, next) => {
-	if (req.user && req.user.role === "admin") {
-		next();
-	} else {
-		return res.status(403).json({ message: "Access denied - Admin only" });
-	}
+    if (req.user && req.user.role === "admin") {
+        next();
+    } else {
+        return res.status(403).json({ message: "Access denied - Admin only" });
+    }
 };
