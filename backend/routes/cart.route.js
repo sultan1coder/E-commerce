5ba57c33c55d5202f5abce739d1ebe299f7dd199
ashraf <sadarsign@gmail.com>
2025-02-21 14:49:24 +0300

    Initialized

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { addToCart } from "../controllers/cart.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getCartProducts)
+router.post("/", protectRoute, addToCart)
+router.delete("/", protectRoute, removeAllFromCart)
+router.put("/", protectRoute, updateQuantity)
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cartRoutes from "./routes/cart.route.js";
 
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
