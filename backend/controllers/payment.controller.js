b96e1767a8ccc1eca19db05cd40b2c2e1c905ad1
ashraf <sadarsign@gmail.com>
2025-02-22 18:38:23 +0300

    Initialized

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -81,3 +81,18 @@ async function createStripeCoupon(discountPercentage) {
 
 	return coupon.id;
 }
+
+async function createNewCoupon(userId) {
+	await Coupon.findOneAndDelete({ userId });
+
+	const newCoupon = new Coupon({
+		code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+		discountPercentage: 10,
+		expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+		userId: userId,
+	});
+
+	await newCoupon.save();
+
+	return newCoupon;
+}
