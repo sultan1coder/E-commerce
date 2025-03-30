4266806135c23a017ab4bb4c4e9c196eb8d28857
ashraf <sadarsign@gmail.com>
2025-03-30 19:36:24 +0300

    Initialized

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,63 +1,63 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "name is required"]
+      type: String,
+      required: [true, "name is required"],
     },
     email: {
-        type: String,
-        required: [true, "email is required"],
-        unique: true,
-        lowercase: true,
-        trim: true
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: [true, "password is required"],
-        minlength: [6, "password must be at least 6 characters long"]
+      type: String,
+      required: [true, "password is required"],
+      minlength: [6, "password must be at least 6 characters long"],
     },
     chartItems: [
-        {
-            quantity: {
-                type: Number,
-                default: 1
-            },
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product"
-            }
-        }
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+      },
     ],
     role: {
-        type: String,
-        enum: ["customer", "admin",],
-        default: "customer"
-    }
-},
-    {
-        timestamps: true,
-    })
-
-
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 //pre-save hook to hash password before saving to database
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next()
+  if (!this.isModified("password")) return next();
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
-})
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 //sulta 1234567
-//1234567 => invalid credentials 
+//1234567 => invalid credentials
 userSchema.methods.comparePassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
 const User = mongoose.model("user", userSchema);
