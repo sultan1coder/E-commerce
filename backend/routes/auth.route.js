be784e93c9a82c16ffd151e43f28cba16c5eb562
ashraf <sadarsign@gmail.com>
2025-03-30 19:36:55 +0300

    Initialized

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { login, logout, signup, refreshToken, getProfile } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  refreshToken,
+  getProfile,
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
