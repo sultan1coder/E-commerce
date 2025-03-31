cd7015edf0aae0b4936690406d280c3ded1fbd0e
ashraf <sadarsign@gmail.com>
2025-03-31 22:33:09 +0300

    Initialized

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,13 +1,13 @@
 const LoadingSpinner = () => {
-	return (
-		<div className='flex items-center justify-center min-h-screen bg-gray-900'>
-			<div className='relative'>
-				<div className='w-20 h-20 border-emerald-200 border-2 rounded-full' />
-				<div className='w-20 h-20 border-emerald-500 border-t-2 animate-spin rounded-full absolute left-0 top-0' />
-				<div className='sr-only'>Loading</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <div className="relative">
+        <div className="w-20 h-20 border-2 rounded-full border-emerald-200" />
+        <div className="absolute top-0 left-0 w-20 h-20 border-t-2 rounded-full border-emerald-500 animate-spin" />
+        <div className="sr-only">Loading</div>
+      </div>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
