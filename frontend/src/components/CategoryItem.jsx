4baba0f901b796b251e68dc05d786d185f324643
ashraf <sadarsign@gmail.com>
2025-03-31 22:31:56 +0300

    Initialized

--- frontend/src/components/CategoryItem.jsx
+++ frontend/src/components/CategoryItem.jsx
@@ -1,25 +1,27 @@
 import { Link } from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
-	return (
-		<div className='relative overflow-hidden h-96 w-full rounded-lg group'>
-			<Link to={"/category" + category.href}>
-				<div className='w-full h-full cursor-pointer'>
-					<div className='absolute inset-0 bg-gradient-to-b from-transparent to-gray-900 opacity-50 z-10' />
-					<img
-						src={category.imageUrl}
-						alt={category.name}
-						className='w-full h-full object-cover transition-transform duration-500 ease-out group-hover:scale-110'
-						loading='lazy'
-					/>
-					<div className='absolute bottom-0 left-0 right-0 p-4 z-20'>
-						<h3 className='text-white text-2xl font-bold mb-2'>{category.name}</h3>
-						<p className='text-gray-200 text-sm'>Explore {category.name}</p>
-					</div>
-				</div>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="relative w-full overflow-hidden rounded-lg h-96 group">
+      <Link to={"/category" + category.href}>
+        <div className="w-full h-full cursor-pointer">
+          <div className="absolute inset-0 z-10 opacity-50 bg-gradient-to-b from-transparent to-gray-900" />
+          <img
+            src={category.imageUrl}
+            alt={category.name}
+            className="object-cover w-full h-full transition-transform duration-500 ease-out group-hover:scale-110"
+            loading="lazy"
+          />
+          <div className="absolute bottom-0 left-0 right-0 z-20 p-4">
+            <h3 className="mb-2 text-2xl font-bold text-white">
+              {category.name}
+            </h3>
+            <p className="text-sm text-gray-200">Explore {category.name}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default CategoryItem;
