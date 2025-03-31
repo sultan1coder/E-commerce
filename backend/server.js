c4ab0123eb7385cc6d1dda0b7cb99eb1e356ff21
ashraf <sadarsign@gmail.com>
2025-03-31 22:29:15 +0300

    Initialized

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,11 @@ import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 
-
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json({ limit: "10mb" })); //allows you to parse the body of the request 
+app.use(express.json({ limit: "10mb" })); //allows you to parse the body of the request
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -26,7 +24,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 
-    connectDB();
+  connectDB();
 });
