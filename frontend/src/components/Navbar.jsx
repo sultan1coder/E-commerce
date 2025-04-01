6635cf72e4baa55aa8a7f25255b3b40e99406d6a
ashraf <sadarsign@gmail.com>
2025-04-01 17:34:50 +0300

    Initialized

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,88 +4,88 @@ import { useUserStore } from "../stores/useUserStore";
 import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
-	const { user, logout } = useUserStore();
-	const isAdmin = user?.role === "admin";
-	const { cart } = useCartStore();
+  const { user, logout } = useUserStore();
+  const isAdmin = user?.role === "admin";
+  const { cart } = useCartStore();
 
-	return (
-		<header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
-			<div className='container mx-auto px-4 py-3'>
-				<div className='flex flex-wrap justify-between items-center'>
-					<Link to='/' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
-						E-Commerce
-					</Link>
+  return (
+    <header className="fixed top-0 left-0 z-40 w-full transition-all duration-300 bg-gray-900 border-b shadow-lg bg-opacity-90 backdrop-blur-md border-emerald-800">
+      <div className="container px-4 py-3 mx-auto">
+        <div className="flex flex-wrap items-center justify-between">
+          <Link
+            to="/"
+            className="flex items-center space-x-2 text-2xl font-bold text-emerald-400"
+          >
+            E-Commerce
+          </Link>
 
-					<nav className='flex flex-wrap items-center gap-4'>
-						<Link
-							to={"/"}
-							className='text-gray-300 hover:text-emerald-400 transition duration-300
-					 ease-in-out'
-						>
-							Home
-						</Link>
-						{user && (
-							<Link
-								to={"/cart"}
-								className='relative group text-gray-300 hover:text-emerald-400 transition duration-300 
-							ease-in-out'
-							>
-								<ShoppingCart className='inline-block mr-1 group-hover:text-emerald-400' size={20} />
-								<span className='hidden sm:inline'>Cart</span>
-								{cart.length > 0 && (
-									<span
-										className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
-									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
-									>
-										{cart.length}
-									</span>
-								)}
-							</Link>
-						)}
-						{isAdmin && (
-							<Link
-								className='bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium
-								 transition duration-300 ease-in-out flex items-center'
-								to={"/secret-dashboard"}
-							>
-								<Lock className='inline-block mr-1' size={18} />
-								<span className='hidden sm:inline'>Dashboard</span>
-							</Link>
-						)}
+          <nav className="flex flex-wrap items-center gap-4">
+            <Link
+              to={"/"}
+              className="text-gray-300 transition duration-300 ease-in-out hover:text-emerald-400"
+            >
+              Home
+            </Link>
+            {user && (
+              <Link
+                to={"/cart"}
+                className="relative text-gray-300 transition duration-300 ease-in-out group hover:text-emerald-400"
+              >
+                <ShoppingCart
+                  className="inline-block mr-1 group-hover:text-emerald-400"
+                  size={20}
+                />
+                <span className="hidden sm:inline">Cart</span>
+                {cart.length > 0 && (
+                  <span
+                    className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
+									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out"
+                  >
+                    {cart.length}
+                  </span>
+                )}
+              </Link>
+            )}
+            {isAdmin && (
+              <Link
+                className="flex items-center px-3 py-1 font-medium text-white transition duration-300 ease-in-out rounded-md bg-emerald-700 hover:bg-emerald-600"
+                to={"/secret-dashboard"}
+              >
+                <Lock className="inline-block mr-1" size={18} />
+                <span className="hidden sm:inline">Dashboard</span>
+              </Link>
+            )}
 
-						{user ? (
-							<button
-								className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-						rounded-md flex items-center transition duration-300 ease-in-out'
-								onClick={logout}
-							>
-								<LogOut size={18} />
-								<span className='hidden sm:inline ml-2'>Log Out</span>
-							</button>
-						) : (
-							<>
-								<Link
-									to={"/signup"}
-									className='bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 
-									rounded-md flex items-center transition duration-300 ease-in-out'
-								>
-									<UserPlus className='mr-2' size={18} />
-									Sign Up
-								</Link>
-								<Link
-									to={"/login"}
-									className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-									rounded-md flex items-center transition duration-300 ease-in-out'
-								>
-									<LogIn className='mr-2' size={18} />
-									Login
-								</Link>
-							</>
-						)}
-					</nav>
-				</div>
-			</div>
-		</header>
-	);
+            {user ? (
+              <button
+                className="flex items-center px-4 py-2 text-white transition duration-300 ease-in-out bg-gray-700 rounded-md hover:bg-gray-600"
+                onClick={logout}
+              >
+                <LogOut size={18} />
+                <span className="hidden ml-2 sm:inline">Log Out</span>
+              </button>
+            ) : (
+              <>
+                <Link
+                  to={"/signup"}
+                  className="flex items-center px-4 py-2 text-white transition duration-300 ease-in-out rounded-md bg-emerald-600 hover:bg-emerald-700"
+                >
+                  <UserPlus className="mr-2" size={18} />
+                  Sign Up
+                </Link>
+                <Link
+                  to={"/login"}
+                  className="flex items-center px-4 py-2 text-white transition duration-300 ease-in-out bg-gray-700 rounded-md hover:bg-gray-600"
+                >
+                  <LogIn className="mr-2" size={18} />
+                  Login
+                </Link>
+              </>
+            )}
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 export default Navbar;
