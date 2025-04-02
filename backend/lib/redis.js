49313f0ecd602cd5a40d6ca022c90e942e7c7629
ashraf <sadarsign@gmail.com>
2025-04-02 16:26:23 +0300

    Initialized

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,7 @@
-import Redis from "ioredis";
+import Redis from "ioredis"
 import dotenv from "dotenv";
 
 dotenv.config();
+
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
-//redis: key-value store
+// await client.set('foo', 'bar');
