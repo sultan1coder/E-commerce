5f993e72ceb2bef4a0b2e8e370530664389777b0
ashraf <sadarsign@gmail.com>
2025-04-02 16:03:45 +0300

    Initialized

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,41 +4,43 @@ import { useProductStore } from "../stores/useProductStore";
 import FeaturedProducts from "../components/FeaturedProducts";
 
 const categories = [
-	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-	{ href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
-	{ href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
-	{ href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
-	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
-	{ href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
-	{ href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+  { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+  { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
+  { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
+  { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
+  { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
 ];
 
 const HomePage = () => {
-	const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+  const { fetchFeaturedProducts, products, isLoading } = useProductStore();
 
-	useEffect(() => {
-		fetchFeaturedProducts();
-	}, [fetchFeaturedProducts]);
+  useEffect(() => {
+    fetchFeaturedProducts();
+  }, [fetchFeaturedProducts]);
 
-	return (
-		<div className='relative min-h-screen text-white overflow-hidden'>
-			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
-				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
-					Explore Our Categories
-				</h1>
-				<p className='text-center text-xl text-gray-300 mb-12'>
-					Discover the latest trends in eco-friendly fashion
-				</p>
+  return (
+    <div className="relative min-h-screen overflow-hidden text-white">
+      <div className="relative z-10 px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1 className="mb-4 text-5xl font-bold text-center sm:text-6xl text-emerald-400">
+          Explore Our Categories
+        </h1>
+        <p className="mb-12 text-xl text-center text-gray-300">
+          Discover the latest trends in eco-friendly fashion
+        </p>
 
-				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-					{categories.map((category) => (
-						<CategoryItem category={category} key={category.name} />
-					))}
-				</div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {categories.map((category) => (
+            <CategoryItem category={category} key={category.name} />
+          ))}
+        </div>
 
-				{!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
-			</div>
-		</div>
-	);
+        {!isLoading && products.length > 0 && (
+          <FeaturedProducts featuredProducts={products} />
+        )}
+      </div>
+    </div>
+  );
 };
 export default HomePage;
